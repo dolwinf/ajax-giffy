6d02f53977c6503714cccaecb9c71d6ca0ef32ef
Dolwin Fernandes <dolwinf@gmail.com>
2019-09-03 16:24:05 +1000

    Setup APIkey

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,25 @@
 var buttonsArray = [
-	"Cat",
-	"Dog",
-	"Goku",
-	"Arnold",
-	"Mike Tyson",
-	"Ricky Pointing",
-	"Supercar",
-	"Nasa",
-	"Games",
-	"Ghost",
-	"Relaxed",
-	"Beach"
+  "Cat",
+  "Dog",
+  "Goku",
+  "Arnold",
+  "Mike Tyson",
+  "Ricky Pointing",
+  "Supercar",
+  "Nasa",
+  "Games",
+  "Ghost",
+  "Relaxed",
+  "Beach"
 ];
+
+var searchTerm = "";
+var query = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=OGnk9LWXYQF38vG8loDkmGDa2Fm7FJgN`;
 
 var buttons = $("#buttons");
 
 buttonsArray.forEach(function(item) {
-	buttons.append(
-		"<button class='btn btn-primary buttons'>" + item + "</button>"
-	);
+  buttons.append(
+    "<button class='btn btn-primary buttons'>" + item + "</button>"
+  );
 });
